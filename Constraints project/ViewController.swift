ccb0b526d569d2ac66d6812300b870275ce21667
Dmitry <borodulkin_d@mail.ru>
2023-03-09 11:11:42 +0300

    Initial Commit

---
+++ Constraints project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constraints project
+//
+//  Created by Дмитрий Бородулькин on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
